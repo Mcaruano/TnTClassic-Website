0f80e955474dcd58e28c54e230860bdcb5fdb32e
Matt Caruano <mjcengineer89@yahoo.com>
2021-02-03 15:52:58 -0800

    Touched up the LootHistory page by including a Tier column and also making the initial link display the item name instead of itemID

--- js/loot-history.js
+++ js/loot-history.js
@@ -21,18 +21,21 @@ function populateLootHistory(lootHistoryJson) {
     var row = table.insertRow(i + 1);
 
     // td0 = Player
-    // td1 = Loot Mode
-    // td2 = Date
-    // td3 = Item
+    // td1 = Date
+    // td2 = Loot Mode
+    // td3 = Tier
+    // td4 = Item
     var playerCell = row.insertCell(0);
-    var lootModeCell = row.insertCell(1);
-    var dateCell = row.insertCell(2);
-    var itemCell = row.insertCell(3);
+    var dateCell = row.insertCell(1);
+    var lootModeCell = row.insertCell(2);
+    var contentTierCell = row.insertCell(3);
+    var itemCell = row.insertCell(4);
 
     // Add some text to the new cells:
     playerCell.innerHTML = lootHistoryJson[i].Recipient;
-    lootModeCell.innerHTML = lootHistoryJson[i].LootMode;
     dateCell.innerHTML = lootHistoryJson[i].Timestamp;
-    itemCell.innerHTML = '<a href="https://classic.wowhead.com/item=' + lootHistoryJson[i].ItemID + '" target="_blank" rel="noopener"> ItemID: ' + lootHistoryJson[i].ItemID + '</a>';
+    lootModeCell.innerHTML = lootHistoryJson[i].LootMode;
+    contentTierCell.innerHTML = lootHistoryJson[i].ItemTier;
+    itemCell.innerHTML = '<a href="https://classic.wowhead.com/item=' + lootHistoryJson[i].ItemID + '" target="_blank" rel="noopener">' + lootHistoryJson[i].ItemName + '</a>';
   }
 }
