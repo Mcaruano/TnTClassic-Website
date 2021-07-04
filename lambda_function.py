43089b3b29d5814ffd5e7f8bb8d72c3da1b7ddf4
Matt Caruano <mjcengineer89@yahoo.com>
2021-07-03 18:38:31 -0700

    Added queryParam handling for the lambda method

--- lambda_function.py
+++ lambda_function.py
@@ -7,9 +7,21 @@ import logging
 Queries the 'Recipient-Timestamp-index' Index of the 'TransactionHistory' DDB table
 for the records pertaining to the given recipient.
 """
-def query_ddb_index(recipientName, reverse=True):
+def query_ddb_index(recipientName, lootMode, reverse=True):
     ddb_client = boto3.client('dynamodb')
     response = None
+
+    filterExpression=''
+    expressionAttrValues = { 
+        ':recipient':{'S': recipientName}
+    }
+    if lootMode == 'PRIORITY':
+        filterExpression = 'LootMode = :lootMode'
+        expressionAttrValues[':lootMode'] = {'S': 'PRIORITY'}
+    elif lootMode == 'LOTTERY':
+        filterExpression = 'LootMode = :lootMode'
+        expressionAttrValues[':lootMode'] = {'S': 'LOTTERY'}
+    
     try:
         response = ddb_client.query(
             TableName='TransactionHistory',
@@ -17,9 +29,8 @@ def query_ddb_index(recipientName, reverse=True):
             Select='ALL_PROJECTED_ATTRIBUTES', # This parameter is only availble if we specify an IndexName
             ScanIndexForward=not reverse, # If True, will return results Ascending (AKA - not reversed)
             KeyConditionExpression='Recipient = :recipient', # Can only reference Partition/Sort keys from Indexes
-            ExpressionAttributeValues= { 
-                ":recipient":{"S": recipientName}
-            }
+            FilterExpression=filterExpression,            
+            ExpressionAttributeValues= expressionAttrValues
         )
     except ClientError as e:
         # Disregard the errors thrown from the ConditionExpression check
@@ -28,14 +39,22 @@ def query_ddb_index(recipientName, reverse=True):
     return response
 
 def lambda_handler(event, context):
-    # TODO: Fetch the recipientName from the QueryParams
-    ddbResults = query_ddb_index('Akaran')
+    playerName = event["queryStringParameters"]["playerName"]
+    lootMode = event["queryStringParameters"]["lootMode"]
+    ddbResults = query_ddb_index(playerName, lootMode)
     
     # Format the Lambda response to adhere to the standard Response structure
     properlyFormattedResponse = {}
     properlyFormattedResponse['isBase64Encoded'] = False
-    properlyFormattedResponse['statusCode'] = 200
     properlyFormattedResponse['headers'] = {'Access-Control-Allow-Origin': '*'}
-    properlyFormattedResponse['body'] = json.dumps(ddbResults['Items'], indent = 2)
+
+    if ddbResults == None or 'Items' not in ddbResults:
+        properlyFormattedResponse['statusCode'] = 500
+        errorBody = {}
+        errorBody['Message'] = "No results found. Please be sure you're using proper case and special characters."
+        properlyFormattedResponse['body'] = json.dumps(errorBody, indent = 2)
+    else:
+        properlyFormattedResponse['statusCode'] = 200    
+        properlyFormattedResponse['body'] = json.dumps(ddbResults['Items'], indent = 2)
 
     return properlyFormattedResponse
